2fd039062bfab1c157b87ce0996d0073882e6c1c
otot <rgotot@icloud.com>
2021-08-16 21:45:41 +0900

    他人の編集ページにアクセスできないようにした。/usersにアクセスできないところ、リンクが機能しないところを修正した

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,12 +6,19 @@ class UsersController < ApplicationController
   def show
     # https://railsguides.jp/action_controller_overview.html
     user_params_id
-    @user = User.find(params[:id]) # ここにnilが渡ってきてNot Foundのエラーが来る可能性があることに配慮したコードである必要がある
+    @user = User.find(params[:id])
+    # ここにnilが渡ってきてNot Foundのエラーが来る可能性があることに配慮したコードである必要がある
   end
 
   def edit
     user_params_id
     @user = User.find(params[:id])
+    if @user == current_user
+      render "edit"
+    else
+      flash[:danger] = "他のユーザーの情報は変更できません"
+      redirect_to "/"
+    end
   end
 
   def update
@@ -28,7 +35,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:id, :name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
   def user_params_id
