318aebf0cbab24b8ecddee158c3b733f47f14110
otot <rgotot@icloud.com>
2021-08-16 16:46:05 +0900

    ユーザー編集ページを作った

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,13 +5,33 @@ class UsersController < ApplicationController
 
   def show
     # https://railsguides.jp/action_controller_overview.html
-    user_params
+    user_params_id
     @user = User.find(params[:id]) # ここにnilが渡ってきてNot Foundのエラーが来る可能性があることに配慮したコードである必要がある
+  end
+
+  def edit
+    user_params_id
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user_params_id
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user
+      # 更新に成功した場合を扱う。
+    else
+      render 'edit'
+    end
   end
 
   private
 
   def user_params
+    params.require(:user).permit(:id, :name, :email, :password, :password_confirmation)
+  end
+
+  def user_params_id
     params.require(:id)
   end
 end
