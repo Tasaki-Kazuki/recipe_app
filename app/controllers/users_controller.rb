8f0f4504413690c5813dc8a28f858f1e8c6d60e4
otot <rgotot@icloud.com>
2021-09-02 21:19:08 +0900

    N+１問題の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     # https://railsguides.jp/action_controller_overview.html
     user_params_id
     @user = User.find(params[:id])
-    @recipes = @user.recipes.page(params[:page]).per(10).reverse_order
+    @recipes = @user.recipes.includes(:favorites).page(params[:page]).per(10).reverse_order
     # ここにnilが渡ってきてNot Foundのエラーが来る可能性があることに配慮したコードである必要がある
   end
 
